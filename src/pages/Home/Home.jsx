632567992510af9b5eb9aa24515858d0b75724aa
komol21 <komolpaul86@gmail.com>
2025-01-25 14:01:40 +0600

    partners updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Home/Banner";
+import PartnersSection from "../../components/Home/PartnersSection";
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <PartnersSection/>
         </div>
     );
 };
